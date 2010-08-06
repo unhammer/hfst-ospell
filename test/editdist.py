57441ea2601322c7ceaff54ef46d14e5eea3c660
Traubert <sam.hardwick@iki.fi>
2010-08-06 12:10:30 +0000

    New test script
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@569 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ test/editdist.py
@@ -0,0 +1,25 @@
+# This is for generating error sources - give an alphabet as the argument,
+# get (currently) editdist-1 without swaps. I use hfst-repeat for bigger
+# distances.
+
+import sys
+
+epsilon = u'<>'
+
+if len(sys.argv) < 2:
+    sys.exit()
+if len(sys.argv) == 2:
+    distance = 1
+else:
+    distance = int(sys.argv[2])
+alphabet = unicode(sys.argv[1], 'utf-8')
+
+for char in alphabet:
+    print "0\t0\t" + char.encode("utf-8") + "\t" + char.encode("utf-8")
+    print "1\t1\t" + char.encode("utf-8") + "\t" + char.encode("utf-8")
+alphabet = [a for a in alphabet] + [epsilon]
+for char in alphabet:
+    temp = [c for c in alphabet if c != char]
+    for c in temp:
+        print "0\t1\t" + char.encode("utf-8") + "\t" + c.encode("utf-8")
+print 1
