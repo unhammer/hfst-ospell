f77baa8b71dfbfbe550fba0dd6545976f89dcc93
Traubert <sam.hardwick@iki.fi>
2010-08-11 17:37:51 +0000

    Improvements to editdist.py - see test/editdist.py --help
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@594 941e2c2b-deac-454f-805a-451daa25f33c

--- test/editdist.py
+++ test/editdist.py
@@ -1,23 +1,40 @@
-# This is for generating error sources - give an alphabet as the argument,
-# get (currently) editdist-1 without swaps. I use hfst-repeat for bigger
-# distances.
+#!/usr/bin/python
+
+# see edisdist.py --help for usage
 
 import sys
+from optparse import OptionParser
 
-epsilon = u'<>'
+usage_string = "usage: %prog [options] alphabet"
+parser = OptionParser(usage=usage_string)
+parser.add_option("-e", "--epsilon", dest = "epsilon",
+                  help = "specify symbol to use as epsilon", metavar = "EPS")
+parser.add_option("-d", "--distance", type = "int", dest = "distance",
+                  help = "specify edit depth", metavar = "DIST")
+parser.set_defaults(epsilon = '@0@')
+parser.set_defaults(distance = 1)
+(options, args) = parser.parse_args()
 
-if len(sys.argv) < 2:
+if len(args) == 0:
+    print "No alphabet specified!"
     sys.exit()
-if len(sys.argv) > 2:
-    epsilon = unicode(sys.argv[2], 'utf-8')
-alphabet = unicode(sys.argv[1], 'utf-8')
+if len(args) > 1:
+    print "Too many options!"
+    sys.exit()
+
+alphabet = unicode(args[0], 'utf-8')
+epsilon = unicode(options.epsilon, 'utf-8')
+distance = options.distance
+
+for state in range(distance):
+    for char in alphabet: # first the nonmodifying transitions
+        print str(state) + "\t" + str(state) + "\t" + char.encode("utf-8") + "\t" + char.encode("utf-8") + "\t0.0"
+    print str(state + 1) + "\t0.0" # all states except the first one are final
+
+alphabet = [a for a in alphabet] + [epsilon] # now add epsilon to the alphabet for the edits
 
 for char in alphabet:
-    print "0\t0\t" + char.encode("utf-8") + "\t" + char.encode("utf-8") + "\t1.0"
-    print "1\t1\t" + char.encode("utf-8") + "\t" + char.encode("utf-8") + "\t1.0"
-alphabet = [a for a in alphabet] + [epsilon]
-for char in alphabet:
-    temp = [c for c in alphabet if c != char]
-    for c in temp:
-        print "0\t1\t" + char.encode("utf-8") + "\t" + c.encode("utf-8") + "\t1.0"
-print "1\t0.0"
+    others = [c for c in alphabet if c != char]
+    for other in others:
+        for state in range(distance):
+            print str(state) + "\t" + str(state + 1) + "\t" + char + "\t" + other + "\t1.0"
