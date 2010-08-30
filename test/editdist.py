c227d79f4b685b5ff7a3929b4033713fbf90a981
Traubert <sam.hardwick@iki.fi>
2010-08-30 19:34:38 +0000

    Diagnostics and info about expected transducer model in test script
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@650 941e2c2b-deac-454f-805a-451daa25f33c

--- test/editdist.py
+++ test/editdist.py
@@ -2,11 +2,22 @@
 
 # see edisdist.py --help for usage
 
+
 import sys
 from optparse import OptionParser
 
 usage_string = "usage: %prog [options] alphabet"
-parser = OptionParser(usage=usage_string)
+
+info_string = """With d for distance and S for size of alphabet plus one (for epsilon),
+expected output is a transducer in ATT format with                              
+* Swapless:                                                                     
+** n + 1 states                                                                
+** n(S^2 + S - 1) transitions                                                  
+* Swapful:                                                                      
+** n(S^2 - 3S + 3) + 1 states                                                  
+** n(3S^2 - 5S + 3) transitions"""                                                 
+
+parser = OptionParser(usage=usage_string, epilog=info_string)
 parser.add_option("-e", "--epsilon", dest = "epsilon",
                   help = "specify symbol to use as epsilon, default is @0@",
                   metavar = "EPS")
@@ -15,9 +26,12 @@ parser.add_option("-d", "--distance", type = "int", dest = "distance",
                   metavar = "DIST")
 parser.add_option("-s", "--swap", action = "store_true", dest="swap",
                   help = "generate swaps (as well as insertions and deletions)")
+parser.add_option("-v", "--verbose", action = "store_true", dest="verbose",
+                  help = "print some diagnostics to standard error")
 parser.set_defaults(epsilon = '@0@')
 parser.set_defaults(distance = 1)
 parser.set_defaults(swap = False)
+parser.set_defaults(verbose = False)
 (options, args) = parser.parse_args()
 
 if len(args) == 0:
@@ -31,6 +45,8 @@ alphabet = unicode(args[0], 'utf-8')
 epsilon = unicode(options.epsilon, 'utf-8')
 distance = options.distance
 swapstate = distance + 1 # used for swap states
+transitions = 0 # transition counter
+chars = len(alphabet) + 1 # alphabet size
 OTHER = u'@?@'
 
 def p(string): # stupid python, or possibly stupid me
@@ -41,6 +57,7 @@ for state in range(distance):
 
         # first the nonmodifying transitions
         print str(state) + "\t" + str(state) + "\t" + p(char) + "\t" + p(char) + "\t0.0"
+	transitions += 1
 
         # then swaps of two characters - we make a separate state counter for this
         if options.swap:
@@ -49,6 +66,7 @@ for state in range(distance):
                 print str(state) + "\t" + str(swapstate) + "\t" + p(char) + "\t" + p(other) + "\t1.0"
                 print str(swapstate) + "\t" + str(state + 1) + "\t" + p(other) + "\t" + p(char) + "\t0.0"
                 swapstate += 1 # make a new state
+		transitions += 2
 
     # all states except swap states and the initial state are final
     print str(state + 1) + "\t0.0"
@@ -61,9 +79,15 @@ for char in alphabet: # generate all insertions, deletions and substitutions
     for state in range(distance):
         for other in others:
             print str(state) + "\t" + str(state + 1) + "\t" + p(char) + "\t" + p(other) + "\t1.0"
+	    transitions += 1
 
 # finally deletions and substitutions of OTHER
 
 for state in range(distance):
     for char in alphabet:
         print str(state) + "\t" + str(state + 1) + "\t" + p(OTHER) + "\t" + p(char) + "\t1.0"
+	transitions += 1
+
+if options.verbose:
+    sys.stderr.write(str(swapstate) + " states and " + str(transitions) + " transitions written for\n"+
+                     "distance " + str(distance) + " and alphabet size " + str(chars) +"\n")
