4f2c186041461872c829b89d30e91959bd7fbbbb
flammie <flammie@iki.fi>
2011-04-25 14:04:36 +0000

    check fread return value as advised by gcc
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@1259 941e2c2b-deac-454f-805a-451daa25f33c

--- hfst-ol.cc
+++ hfst-ol.cc
@@ -37,7 +37,10 @@ void TransducerHeader::skip_hfst3_header(FILE * f)
 	    throw HeaderParsingException();
 	}
 	char * headervalue = new char[remaining_header_len];
-	fread(headervalue, remaining_header_len, 1, f);
+	if (fread(headervalue, remaining_header_len, 1, f) < remaining_header_len)
+      {
+        throw HeaderParsingException();
+      }
 	if (headervalue[remaining_header_len - 1] != '\0') {
 	    throw HeaderParsingException();
 	}
