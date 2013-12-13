58047b3058ad59220da40da10d601daa02ec76ae
flammie <flammie@iki.fi>
2013-12-13 00:48:30 +0000

    index.xml with empty descriptions
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@3642 941e2c2b-deac-454f-805a-451daa25f33c

--- empty-descriptions.sh
+++ empty-descriptions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -x ./hfst-ospell ; then
-    if ! cat $srcdir/test.strings | ./hfst-ospell speller_basic.zhfst ; then
+    if ! cat $srcdir/test.strings | ./hfst-ospell -v empty_descriptions.zhfst ; then
         exit 1
     fi
 else
