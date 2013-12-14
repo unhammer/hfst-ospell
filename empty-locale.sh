42da63d9a9920a1c3dd6d68449d6302a3b983f3c
snomos <snomos@users.noreply.github.com>
2013-12-14 13:10:06 +0000

    Data and shell scripts to test empty titles and empty locale.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@3649 941e2c2b-deac-454f-805a-451daa25f33c

---
+++ empty-locale.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if test -x ./hfst-ospell ; then
+    if ! cat $srcdir/test.strings | ./hfst-ospell -v empty_locale.zhfst ; then
+        exit 1
+    fi
+else
+    echo ./hfst-ospell not built
+    exit 73
+fi
+
