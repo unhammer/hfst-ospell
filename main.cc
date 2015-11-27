cf8ee5cd31c0cb0ae31abaa151c66aed8ff51e77
eaxelson <eaxelson@users.noreply.github.com>
2015-11-27 15:52:50 +0000

    Fix a typo in wide string conversion function.
    git-svn-id: svn://svn.code.sf.net/p/hfst/code/trunk@4552 941e2c2b-deac-454f-805a-451daa25f33c

--- main.cc
+++ main.cc
@@ -61,7 +61,7 @@ static bool suggest_reals = false;
 #ifdef WINDOWS
 static std::string wide_string_to_string(const std::wstring & wstr)
 {
-  int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)§wstr.size(), NULL, 0, NULL, NULL);
+  int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
   std::string str( size_needed, 0 );
   WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &str[0], size_needed, NULL, NULL);
   return str;
