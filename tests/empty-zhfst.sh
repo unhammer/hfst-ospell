2a4e55602f41403974821ceb6a4db0ab6d1971e3
Tino Didriksen <tino@didriksen.cc>
2017-01-10 16:50:06 +0000

    Move more files

--- tests/empty-zhfst.sh
+++ tests/empty-zhfst.sh
@@ -2,7 +2,7 @@
 
 if test -x ./hfst-ospell ; then
     rm -f empty.zhfst
-	touch empty.zhfst
+    touch empty.zhfst
     if ! cat $srcdir/tests/test.strings | ./hfst-ospell -v empty.zhfst ; then
         exit 1
     fi
